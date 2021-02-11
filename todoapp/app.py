35ddeef36e65b86d1398b2e1338e9be9d64a6485
Preston Kresch <ptkresch@gmail.com>
2021-02-10 17:15:46 -0800

    Add AJAX implemenation

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,13 +17,28 @@ class Todo(db.Model):
 db.create_all()
 
 
+#AJAX implementation
 @app.route('/todos/create', methods=['POST'])
 def create_todo(): 
-    description = request.form.get('description', '')
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+        'description': todo.description
+    })
+
+
+#Form submission
+# @app.route('/todos/create', methods=['POST'])
+# def create_todo(): 
+#     description = request.form.get('description', '')
+#     todo = Todo(description=description)
+#     db.session.add(todo)
+#     db.session.commit()
+#     return redirect(url_for('index'))
+
+
 #index looks for this default path
 @app.route('/')
 
