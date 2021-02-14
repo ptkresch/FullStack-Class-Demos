3e8574922a570849d6f91c6d0e3f9a201208ff6f
Preston Kresch <ptkresch@gmail.com>
2021-02-14 00:31:50 -0800

    Add endpoint for delete todo, add click handler for delete button

--- todoapp/app.py
+++ todoapp/app.py
@@ -58,6 +58,18 @@ def set_completed_todo(todo_id):
         db.session.close()
     return redirect(url_for('index'))
 
+@app.route('/todos/<todo_id>/delete-todo', methods=['GET'])
+def delete_todo(todo_id):
+    try:
+        todo = Todo.query.get(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 #Form submission
 # @app.route('/todos/create', methods=['POST'])
 # def create_todo(): 
